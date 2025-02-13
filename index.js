efdf6bf5bdc503bce66e9d994c464ff5dbae8d4c
kuldeepchavda <kuldeep2231qk@gmail.com>
2025-02-13 18:50:54 +0530

    Removed node_modules and .env from repository

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const PORT = process.env.PORT || 5000;
 const YT_API_KEY = process.env.YT_API_KEY;
 
 app.use("/playlist", playlistRoutes);
+
+
 app.use("/demo", async(req,res)=>{
   res.send("This is working");
 });
