b8f6151b8ca0fb8132d8f577cb885632f3fde12a
kuldeepchavda <kuldeep2231qk@gmail.com>
2025-02-14 22:49:30 +0530

    added playlists list

--- models/Playlists.js
+++ models/Playlists.js
@@ -1,10 +1,10 @@
 const mongoose =require("mongoose")
 const playlistSchema = new mongoose.Schema({
   playlistId: { type: String, required: true, unique: true },
-  videos: [{ id: String, watched: { type: Boolean, default: false },thumbnail:{default:String,medium:String,large:String,standard:String} }],
+  videos: [{ id: String, watched: { type: Boolean, default: false },thumbnail:{default:String,medium:String,large:String,standard:String},title:String }],
   total: { type: Number, required: true },
   completed: { type: Number, default: 0 },
 });
 
-const Playlist = mongoose.model("Playlist_5", playlistSchema);
+const Playlist = mongoose.model("Playlist_511", playlistSchema);
 module.exports=Playlist
